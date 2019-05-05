220b4c3c7b2bf494551fa251262569566329854f
Jin Joo Lee <jjl642@nyu.edu>
2019-05-05 14:13:48 -0400

    transportation section

--- APT Saver/Controllers/ListingDetailTableViewController.swift
+++ APT Saver/Controllers/ListingDetailTableViewController.swift
@@ -19,7 +19,7 @@ class ListingDetailTableViewController: UITableViewController {
     @IBOutlet weak var listingSizeLabel: UILabel!
     @IBOutlet weak var listingPpsqftLabel: UILabel!
     @IBOutlet weak var listingAmenitiesLabel: UILabel!
-    @IBOutlet weak var listingTransportationLabel: UILabel!
+   //  @IBOutlet weak var listingTransportationLabel: UILabel!
     
     //    var listing: Listing? = ListingType.getListingTypes()[0].listings[0]
     var listing: Listing?
@@ -27,6 +27,8 @@ class ListingDetailTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Edit Listing"
+        transportCollectionView.delegate = self
+        transportCollectionView.dataSource = self
         
         listingImageView.image = listing?.image
         listingTitleTextField.text  = listing?.address
@@ -37,10 +39,32 @@ class ListingDetailTableViewController: UITableViewController {
         listingSizeLabel.text = listing?.size
         listingPpsqftLabel.text = listing?.ppsqft
         listingAmenitiesLabel.text = listing?.amenities
-        listingTransportationLabel.text = listing?.transportation
+        // listingTransportationLabel.text = listing?.transportation
         
     }
     
+    @IBOutlet weak var transportCollectionView: UICollectionView!
+    
+}
+
+var dummyTransportOPtions: [[String]] = [["A", "E", "C"], ["N", "Q"], ["R", "W", "B", "F"]]
+
+extension ListingDetailTableViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dummyTransportOPtions[section].count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = transportCollectionView.dequeueReusableCell(withReuseIdentifier: "transportCellId", for: indexPath) as! TransportCollectionViewCell
+        cell.transportLetter.text = dummyTransportOPtions[indexPath.section][indexPath.row]
+        return cell
+        
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return dummyTransportOPtions.count
+    }
 }
 
 
