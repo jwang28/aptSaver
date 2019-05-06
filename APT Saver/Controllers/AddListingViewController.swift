5ea9ba8420b83f0135c81bb1b2e9d7a888e9f7f2
Jin Joo Lee <jjl642@nyu.edu>
2019-05-06 19:48:21 -0400

    Calendar basic setup. Detail view anti-scrapping prevention hacked by using a UIWebView.

--- APT Saver/Controllers/AddListingViewController.swift
+++ APT Saver/Controllers/AddListingViewController.swift
@@ -15,11 +15,30 @@ protocol AddListingViewControllerDelegate {
 
 class AddListingViewController: UIViewController {
     
+    @IBOutlet weak var webView: UIWebView!
+    
     var delegate: AddListingViewControllerDelegate?
 
     @IBAction func SubmitButton(_ sender: Any) {
+        
+        // TODO: remove this line of code (testing URL)
+        url.text = "https://streeteasy.com/building/the-continental-885-6-avenue-new_york/35g"
+        
         if url.text != ""{
             let url_new = URL(string: self.url.text!)
+            
+            webView.loadRequest(URLRequest(url: url_new!))
+            
+            print("URL: \(url_new!)")
+            
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 4.0) {
+                let string = self.webView.stringByEvaluatingJavaScript(from: "document.body.innerHTML")!
+                print("string: \(string)")
+                self.process(htmlContent: string)
+            }
+            
+            return
+            
             //let url_new = URL(string: "https://streeteasy.com/building/mantena-431-west-37-street-new_york/2a?similar=1")
             
             let task = URLSession.shared.dataTask(with: url_new!) { (data, response, error) in
@@ -27,62 +46,66 @@ class AddListingViewController: UIViewController {
                     print (error)
                 } else {
                     let htmlContent = String(data: data!,encoding: String.Encoding.utf8)!
-                    do {
-                        let doc: Document = try SwiftSoup.parseBodyFragment(htmlContent)
-                        self.addressTest = try doc.getElementsByClass("building-title").text()
-                        self.price = try doc.getElementsByClass("price").text()
-                        self.descriptionText = try doc.getElementsByClass("Description-block jsDescriptionExpanded").text()
-                       
-                        let details: [Element] = try (doc.getElementsByClass("details_info").first()?.children().array())!
-                        for index in 0...details.count - 1 {
-                            
-                            let temp = try details[index].text()
-                            if self.bed != " "{
-                                self.bed = self.bed + " | " + temp
-                            } else {
-                                self.bed = temp
-                            }
-                        }
-                        
-                        let amenitiesHi: [Element] = try doc.getElementsByClass("AmenitiesBlock-highlightsItem").array()
-                        for index in 0...amenitiesHi.count - 1  {
-                            let temp = try amenitiesHi[index].text()
-                            if self.amenities != " " {
-                                self.amenities = self.amenities + " | " + temp
-                            } else {
-                                self.amenities = temp
-                            }
-                            print(self.amenities)
-                        }
-                        
-                        let jpgs: Elements? = try doc.getElementById("carousel")?.select("img[src$=.jpg]")
-                        
-                        print("jpgs", try jpgs?.array())
-                        print("get images", try jpgs?.get(1).attr("src"), "end")
-                        try self.downloadImage(with: URL(string: (jpgs?.get(0).attr("src"))!)!)
-                        
-                        let transportation: Element = try doc.getElementsByClass("Nearby-transportationList").get(0)
-                        self.transportation = try transportation.text()
-                        
-                        
-                    } catch Exception.Error (let type, let message) {
-                        print(type,message)
-                        print(message)
-                    } catch {
-                        print("")
-                    }
+                    self.process(htmlContent: htmlContent)
                 }
             }
             task.resume()
         }
-        let delaySeconds = 2.0
-        DispatchQueue.main.asyncAfter(deadline: .now() + delaySeconds) {
+    }
+    
+    func process(htmlContent: String) {
+        do {
+            print("htmlContent: \(htmlContent)")
+            
+            let doc: Document = try SwiftSoup.parseBodyFragment(htmlContent)
+            self.addressTest = try doc.getElementsByClass("building-title").text()
+            self.price = try doc.getElementsByClass("price").text()
+            self.descriptionText = try doc.getElementsByClass("Description-block jsDescriptionExpanded").text()
+            
+            let details: [Element] = try (doc.getElementsByClass("details_info").first()?.children().array())!
+            for index in 0...details.count - 1 {
+                
+                let temp = try details[index].text()
+                if self.bed != " "{
+                    self.bed = self.bed + " | " + temp
+                } else {
+                    self.bed = temp
+                }
+            }
+            
+            let amenitiesHi: [Element] = try doc.getElementsByClass("AmenitiesBlock-highlightsItem").array()
+            for index in 0...amenitiesHi.count - 1  {
+                let temp = try amenitiesHi[index].text()
+                if self.amenities != " " {
+                    self.amenities = self.amenities + " | " + temp
+                } else {
+                    self.amenities = temp
+                }
+                print(self.amenities)
+            }
+            
+            let jpgs: Elements? = try doc.getElementById("carousel")?.select("img[src$=.jpg]")
+            
+            print("jpgs", try jpgs?.array())
+            print("get images", try jpgs?.get(1).attr("src"), "end")
+            try self.downloadImage(with: URL(string: (jpgs?.get(0).attr("src"))!)!)
+            
+            let transportation: Element = try doc.getElementsByClass("Nearby-transportationList").get(0)
+            self.transportation = try transportation.text()
+            
             self.dismiss(animated: true, completion: {
                 self.listings?.append(Listing(address: self.addressTest, price: self.price ,description: self.descriptionText , bed: self.bed, bath: "", size: "", ppsqft: "", amenities: self.amenities, transportation: self.transportation, imageName: self.images!, favorited: false))
                 self.delegate?.didFinishLoadingData(listings:  self.listings!)
             })
+            
+        } catch Exception.Error (let type, let message) {
+            print(type,message)
+            print(message)
+        } catch {
+            print("")
         }
     }
+    
     
     @IBOutlet weak var url: UITextField!
     var images = UIImage(named: "default")
--- APT Saver/Controllers/ListingTableViewController.swift
+++ APT Saver/Controllers/ListingTableViewController.swift
@@ -9,8 +9,33 @@
 import UIKit
 import SwiftSoup
 import GoogleSignIn
+import VACalendar
 
 class ListingTableViewController: UITableViewController {
+    
+    @IBOutlet weak var tableHeaderView: UIView!
+    @IBOutlet weak var monthHeaderView: VAMonthHeaderView! {
+        didSet {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "LLLL"
+            let appereance = VAMonthHeaderViewAppearance(
+                previousButtonImage: UIImage(named: "favHeart")!,
+                nextButtonImage: UIImage(named: "favHeart")!,
+                dateFormatter: formatter
+            )
+            monthHeaderView.delegate = self
+            monthHeaderView.appearance = appereance
+        }
+    }
+    
+    @IBOutlet weak var weekDaysView: VAWeekDaysView! {
+        didSet {
+            let appereance = VAWeekDaysViewAppearance(symbolsType: .veryShort, calendar: defaultCalendar)
+            weekDaysView.appearance = appereance
+        }
+    }
+    
+    var calendarView : VACalendarView!
     
     // MARK: - UITableView
     //MARK: -Data model
@@ -38,6 +63,49 @@ class ListingTableViewController: UITableViewController {
         signoutButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0).isActive = true
         signoutButton.setTitleColor(.black, for: .normal)
         signoutButton.addTarget(self, action: #selector(logOut), for: .touchUpInside)
+    
+        let calendar = VACalendar(calendar: defaultCalendar)
+        calendarView = VACalendarView(frame: .zero, calendar: calendar)
+        calendarView.showDaysOut = true
+        calendarView.selectionStyle = .multi
+        calendarView.monthDelegate = monthHeaderView
+        calendarView.dayViewAppearanceDelegate = self
+        calendarView.monthViewAppearanceDelegate = self
+        calendarView.calendarDelegate = self
+        calendarView.scrollDirection = .horizontal
+        tableHeaderView.addSubview(calendarView)
+        
+        reloadEvents()
+    }
+    
+    func reloadEvents() {
+        calendarView.setSupplementaries([
+            (Date().addingTimeInterval(-(60 * 60 * 70)), [VADaySupplementary.bottomDots([.red, .magenta])]),
+            (Date().addingTimeInterval((60 * 60 * 110)), [VADaySupplementary.bottomDots([.red])]),
+            (Date().addingTimeInterval((60 * 60 * 370)), [VADaySupplementary.bottomDots([.blue, .darkGray])]),
+            (Date().addingTimeInterval((60 * 60 * 430)), [VADaySupplementary.bottomDots([.orange, .purple, .cyan])])
+            ])
+    }
+    
+    let defaultCalendar: Calendar = {
+        var calendar = Calendar.current
+        calendar.firstWeekday = 1
+        calendar.timeZone = TimeZone(secondsFromGMT: 0)!
+        return calendar
+    }()
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        if calendarView.frame == .zero {
+            calendarView.frame = CGRect(
+                x: 0,
+                y: weekDaysView.frame.maxY,
+                width: tableHeaderView.frame.width,
+                height: tableHeaderView.frame.height - weekDaysView.frame.maxY
+            )
+            calendarView.setup()
+        }
     }
     
     @objc func logOut()
@@ -187,3 +255,89 @@ extension ListingTableViewController: AddListingViewControllerDelegate {
         self.tableView.reloadData()
     }
 }
+
+
+extension ListingTableViewController: VAMonthHeaderViewDelegate {
+    
+    func didTapNextMonth() {
+        calendarView.nextMonth()
+    }
+    
+    func didTapPreviousMonth() {
+        calendarView.previousMonth()
+    }
+    
+}
+
+extension ListingTableViewController: VAMonthViewAppearanceDelegate {
+    
+    func leftInset() -> CGFloat {
+        return 10.0
+    }
+    
+    func rightInset() -> CGFloat {
+        return 10.0
+    }
+    
+    func verticalMonthTitleFont() -> UIFont {
+        return UIFont.systemFont(ofSize: 16, weight: .semibold)
+    }
+    
+    func verticalMonthTitleColor() -> UIColor {
+        return .black
+    }
+    
+    func verticalCurrentMonthTitleColor() -> UIColor {
+        return .red
+    }
+    
+}
+
+extension ListingTableViewController: VADayViewAppearanceDelegate {
+    
+    func textColor(for state: VADayState) -> UIColor {
+        switch state {
+        case .out:
+            return UIColor(red: 214 / 255, green: 214 / 255, blue: 219 / 255, alpha: 1.0)
+        case .selected:
+            return .white
+        case .unavailable:
+            return .lightGray
+        default:
+            return .black
+        }
+    }
+    
+    func textBackgroundColor(for state: VADayState) -> UIColor {
+        switch state {
+        case .selected:
+            return .red
+        default:
+            return .clear
+        }
+    }
+    
+    func shape() -> VADayShape {
+        return .circle
+    }
+    
+    func dotBottomVerticalOffset(for state: VADayState) -> CGFloat {
+        switch state {
+        case .selected:
+            return 2
+        default:
+            return -7
+        }
+    }
+    
+}
+
+extension ListingTableViewController: VACalendarViewDelegate {
+    
+    func selectedDates(_ dates: [Date]) {
+        calendarView.startDate = dates.last ?? Date()
+        print(dates)
+    }
+    
+}
+
